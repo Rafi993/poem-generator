c968e6e0bc010307d027ef0cea1e06a2cbe35a71
rafi993 <rafi993@protonmail.com>
2019-12-25 22:46:11 +0530

    Adding poem generator

--- index.js
+++ index.js
@@ -1,8 +1,50 @@
-import { random, randomElement } from './utils';
+import { random, randomElement, capitalize } from './utils';
+
+const whiteSpace = ['.', ',', '\n', ';', ':', '--'];
+const numberOfWords = 200;
+const maxWordsPerLine = 80;
 
 fetch(`${window.location.origin}/ngrams.json`)
   .then(res => res.json())
-  .then(ngrams => {});
+  .then(ngrams => {
+    const keys = Object.keys(ngrams).filter(
+      word => ![...whiteSpace, 'an', 'the'].includes(word)
+    );
 
-// document.getElementById('title').innerText = '';
-// document.getElementById('sonnet').innerHTML = ;
+    let currentWord = keys[random(0, keys.length)];
+    document.getElementById('title').innerHTML = currentWord;
+
+    let poem = '';
+    let line = '';
+
+    for (let i = 0; i < numberOfWords; i++) {
+      const possibleWords = ngrams[currentWord];
+      if (possibleWords) {
+        if (i === 0) {
+          // Capitalize first word in poem
+          line += capitalize(currentWord);
+        }
+
+        currentWord = randomElement(possibleWords);
+        if (currentWord) {
+          if (whiteSpace.includes(currentWord)) {
+            // Join white space chars without space
+
+            line = line + currentWord;
+          } else {
+            line = line + ' ' + currentWord;
+          }
+
+          if (line.length > maxWordsPerLine) {
+            poem += line + '<br/>';
+            line = '';
+          }
+        }
+      }
+    }
+
+    // Adding last line
+    poem += line;
+
+    document.getElementById('poem').innerHTML = poem;
+  });
--- utils.js
+++ utils.js
@@ -2,3 +2,6 @@ export const random = (min, max) => Math.floor(Math.random() * max) + min;
 
 export const randomElement = array =>
   array[Math.floor(Math.random() * array.length)];
+
+export const capitalize = string =>
+  string.charAt(0).toUpperCase() + string.slice(1);
